96b699ff05244315dc1345e0db1c55e121ea3aa1
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-27 16:45:05 +0545

    [update] Renames `parameter` to `arg_type`

--- lowdash/_utils.py
+++ lowdash/_utils.py
@@ -8,16 +8,16 @@ def args_type_checker(func: _T) -> _T:
         annotations = func.__annotations__.copy()
         annotations.pop("return")
 
-        for index, (arg, parameter) in enumerate(annotations.items()):
+        for index, (arg, arg_type) in enumerate(annotations.items()):
             try:
-                if not isinstance(args[index], parameter):
+                if not isinstance(args[index], arg_type):
                     raise TypeError(
-                        f"[lowdash.{func.__name__}] Expected {parameter} for parameter {arg}"
+                        f"[lowdash.{func.__name__}] Expected {arg_type} for parameter {arg}"
                     )
             except KeyError:
-                if not isinstance(kwargs[arg], parameter):
+                if not isinstance(kwargs[arg], arg_type):
                     raise TypeError(
-                        f"[lowdash.{func.__name__}] Expected {parameter} for parameter {arg}"
+                        f"[lowdash.{func.__name__}] Expected {arg_type} for parameter {arg}"
                     )
 
         return func(*args, **kwargs)
