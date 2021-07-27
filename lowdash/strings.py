703e34e06905175781c62a6ada6110760b465821
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-27 16:40:54 +0545

    [update] Uses `@args_type_cheker` in `strings.py`

--- lowdash/strings.py
+++ lowdash/strings.py
@@ -1,10 +1,11 @@
 from .arrays import chunks as array_chunks
 from .arrays import shuffle
 
+from ._utils import args_type_checker
 
+
+@args_type_checker
 def mock(string: str) -> str:
-    if not isinstance(string, str):
-        raise TypeError("[lowdash.strings] Argument must be of type string")
     chunk = list(string)
     for i in range(len(chunk)):
         if i % 2 == 0:
@@ -13,41 +14,31 @@ def mock(string: str) -> str:
     return "".join(chunk)
 
 
+@args_type_checker
 def upper(string: str) -> str:
-    if not isinstance(string, str):
-        raise TypeError("[lowdash.strings] Argument must be of type string")
     return string.upper()
 
 
+@args_type_checker
 def lower(string: str) -> str:
-    if not isinstance(string, str):
-        raise TypeError("[lowdash.strings] Argument must be of type string")
     return string.lower()
 
 
+@args_type_checker
 def scramble(string: str) -> str:
-    if not isinstance(string, str):
-        raise TypeError("[lowdash.strings] Argument must be of type string")
-
     return "".join(shuffle(list(string)))
 
 
+@args_type_checker
 def proper_case(string: str) -> str:
-    if not isinstance(string, str):
-        raise TypeError("[lowdash.strings] Argument must be of type string")
     chunks = string.split(" ")
     for i in range(len(chunks)):
         chunks[i] = chunks[i].capitalize()
     return " ".join(chunks)
 
 
+@args_type_checker
 def shorten(string: str, length: int, sep="...") -> str:
-    if not isinstance(string, str):
-        raise TypeError("[lowdash.strings] Argument string must be of type string")
-    if not isinstance(length, int):
-        raise TypeError("[lowdash.strings] Argument length must be of type int")
-    if not isinstance(sep, str):
-        raise TypeError("[lowdash.strings] Argument sep must be of type string")
     if length > len(string):
         raise ValueError(
             "[lowdash.strings] Argument length must be less than string length"
@@ -55,13 +46,8 @@ def shorten(string: str, length: int, sep="...") -> str:
     return substr(string, 0, length) + sep
 
 
+@args_type_checker
 def substr(string: str, start: int, end: int) -> str:
-    if not isinstance(string, str):
-        raise TypeError("[lowdash.strings] Argument string must be of type string")
-    if not isinstance(start, int):
-        raise TypeError("[lowdash.strings] Argument start must be of type int")
-    if not isinstance(end, int):
-        raise TypeError("[lowdash.strings] Argument end must be of type int")
     if start > len(string):
         raise ValueError(
             "[lowdash.strings] Argument start must be less than string length"
@@ -73,9 +59,6 @@ def substr(string: str, start: int, end: int) -> str:
     return string[start:end]
 
 
+@args_type_checker
 def chunks(string: str, size: int) -> str:
-    if not isinstance(string, str):
-        raise TypeError("[lowdash.strings] Argument string must be of type string")
-    if not isinstance(size, int):
-        raise TypeError("[lowdash.strings] Argument size must be of type int")
     return array_chunks(list(string), size)
