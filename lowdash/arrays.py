c2d2ebf0a188ab7b0f9f6de82ed5ba9f33d998bd
null <60427892+vierofernando@users.noreply.github.com>
2021-08-19 16:13:26 +0700

    refactor: fix codeblock examples and improvements in code

--- lowdash/arrays.py
+++ lowdash/arrays.py
@@ -9,7 +9,7 @@ def chunks(array: list, size: int) -> list:
     Splits an array into chunks of size
     ```py
     >>> lowdash.chunks([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3)
-    >>> [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10]]
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10]]
     ```
     """
     return [array[i : i + size] for i in range(0, len(array), size)]
@@ -20,7 +20,7 @@ def compact(array: list) -> list:
     """Removes all Falsey values from an array
     ```py
     >>> lowdash.compact([1, 2, 0, False, True, 0])
-    >>> [1, 2, True]
+    [1, 2, True]
     ```
     """
     return [i for i in array if i]
@@ -32,7 +32,7 @@ def concat(array: list, *args) -> list:
     Concatenates all Arrays passed to the function
     ```py
     >>> lowdash.concat([1, 2, 3], [4, 5, 6])
-    >>> [1, 2, 3, 4, 5, 6]
+    [1, 2, 3, 4, 5, 6]
     ```
     """
     return array + flatten(list(args))
@@ -44,7 +44,7 @@ def difference(array_a: list, array_b: list) -> list:
     Removes all values that are in the args from the array
     ```py
     >>> lowdash.difference([1, 2, 3, 4, 5], [2, 3, 4])
-    >>> [1, 5]
+    [1, 5]
     ```
     """
     return [i for i in array_a if i not in flatten(list(array_b))]
@@ -56,7 +56,7 @@ def drop(array: list, n: int) -> list:
     Removes the nth element from the array
     ```py
     >>> lowdash.drop([1, 2, 3, 4, 5], 2)
-    >>> [1, 3, 4, 5]
+    [1, 3, 4, 5]
     ```
     """
     array.pop(n)
@@ -69,7 +69,7 @@ def drop_right(array: list, n: int) -> len:
     Similar to drop, but drops the nth element from the right
     ```py
     >>> lowdash.drop_right([1, 2, 3, 4, 5], 2)
-    >>> [1, 3, 4]
+    [1, 3, 4]
     ```
     """
     length = len(array)
@@ -87,7 +87,7 @@ def fill(array: list, value, start: int, end: int) -> list:
     Fills the array with the value passed in
     ```py
     >>> lowdash.fill([0,1,3,4,5],8,0,2)
-    >>> [8, 8, 3, 4, 5]
+    [8, 8, 3, 4, 5]
     ```
     """
     if start < 0:
@@ -132,16 +132,13 @@ def find_index(array: list, fn) -> int:
     Returns the index of the first element in the array that matches the function
     ```py
     >>> lowdash.find_index([1, 2, 3, 4, 5], lambda x: x == 3)
-    >>> 2
+    2
     """
-    index = -1
-
     for i in range(len(array)):
         if fn(array[i]):
-            index = i
-            break
+            return i
 
-    return index
+    return -1
 
 
 @args_type_checker
@@ -150,16 +147,14 @@ def find_last_index(array: list, fn) -> int:
     Similar to findIndex, but finds the last index of the first element that matches the function
     ```py
     >>> lowdash.find_last_index([1, 2, 3, 4, 5], lambda x: x == 3)
-    >>> 4
+    4
     ```
     """
-    index = -1
     for i in range(len(array)):
         if fn(array[i]):
-            index = len(array) - i - 1
-            break
+            return len(array) - i - 1
 
-    return index
+    return -1
 
 
 @args_type_checker
@@ -168,7 +163,7 @@ def index_of(array: list, value) -> int:
     Returns the index of the first element in the array that matches the value
     ```py
     >>> lowdash.index_of([1, 2, 3, 4, 5], 3)
-    >>> 2
+    2
     ```
     """
     try:
@@ -183,7 +178,7 @@ def insert(array: list, index: int, value) -> list:
     Inserts the value into the array at the index passed in
     ```py
     >>> lowdash.insert([1, 2, 3, 4, 5], 2, 6)
-    >>> [1, 2, 6, 3, 4, 5]
+    [1, 2, 6, 3, 4, 5]
     ```
     """
     if not isinstance(array, list):
@@ -199,7 +194,7 @@ def from_pairs(array: list) -> dict:
     Converts a list of pairs into a dictionary
     ```py
     >>> lowdash.from_pairs([("a", 1), ("b", 2), ("c", 3)])
-    >>> {"a": 1, "b": 2, "c": 3}
+    {"a": 1, "b": 2, "c": 3}
     ```
     """
     pairs = {}
@@ -216,13 +211,10 @@ def head(array: list):
     Returns the first element of the array
     ```py
     >>> lowdash.head([1, 2, 3, 4, 5])
-    >>> 1
+    1
     ```
     """
-    if len(array) == 0:
-        return None
-
-    return array[0]
+    return array[0] if len(array) else None
 
 
 @args_type_checker
@@ -231,7 +223,7 @@ def intersection(array: list, *args) -> list:
     Returns the intersection of the arrays passed in
     ```py
     >>> lowdash.intersection([1, 2, 3, 4, 5], [2, 3, 4, 5, 6])
-    >>> [2, 3, 4, 5]
+    [2, 3, 4, 5]
     ```
     """
     return list(
@@ -247,7 +239,7 @@ def join(array: list, delimiter: str) -> str:
     Joins the elements of the array into a string
     ```py
     >>> lowdash.join([1, 2, 3, 4, 5], ",")
-    >>> "1,2,3,4,5"
+    "1,2,3,4,5"
     ```
     """
     return delimiter.join(array)
@@ -259,7 +251,7 @@ def last(array: list):
     Returns the last element of the array
     ```py
     >>> lowdash.last([1, 2, 3, 4, 5])
-    >>> 5
+    5
     ```
     """
     return array[:-1]
@@ -271,7 +263,7 @@ def nth(array: list, index: int):
     Returns the nth element of the array
     ```py
     >>> lowdash.nth([1, 2, 3, 4, 5], 2)
-    >>> 3
+    3
     ```
     """
     return array[index]
@@ -283,7 +275,7 @@ def pull(array: list, *args) -> list:
     Removes the elements of the array passed in from the array
     ```py
     >>> lowdash.pull([1, 2, 3, 4, 5], 2)
-    >>> [1, 3, 4, 5]
+    [1, 3, 4, 5]
     ```
     """
     return [i for i in array if i not in args]
@@ -295,9 +287,9 @@ def remove(array: list, fn) -> list:
     Removes the elements of the array that match the function passed in
     ```py
     >>> lowdash.remove([1, 2, 3, 4, 5], lambda x: x % 2 == 0)
-    >>> [1, 3, 5]
+    [1, 3, 5]
     """
-    return [i for i in array if fn(i) == False]
+    return [i for i in array if not fn(i)]
 
 
 @args_type_checker
@@ -306,11 +298,10 @@ def reverse(array: list) -> list:
     Reverses an array
     ```py
     >>> lowdash.reverse([1, 2, 3, 4, 5])
-    >>> [5, 4, 3, 2, 1]
+    [5, 4, 3, 2, 1]
     ```
     """
-    new_list = list(array).reverse()
-    return new_list
+    return list(array).reverse()
 
 
 @args_type_checker
@@ -319,7 +310,7 @@ def slice(array: list, start: int, end: int) -> list:
     Returns a slice of the array
     ```py
     >>> lowdash.slice([1, 2, 3, 4, 5], 1, 3)
-    >>> [2, 3]
+    [2, 3]
     ```
     """
     if start < 0 or end < 0:
@@ -337,13 +328,10 @@ def tail(array: list) -> list:
     Removes the first element of the array
     ```py
     >>> lowdash.tail([1, 2, 3, 4, 5])
-    >>> [2, 3, 4, 5]
+    [2, 3, 4, 5]
     ```
     """
-    if len(array) < 2:
-        return []
-
-    return array[1:]
+    return [] if len(array) < 2 else array[1:]
 
 
 @args_type_checker
@@ -352,7 +340,7 @@ def take(array: list, till: int = 0) -> list:
     Returns a slice of the array
     ```py
     >>> lowdash.take([1, 2, 3, 4, 5], 3)
-    >>> [1, 2, 3]
+    [1, 2, 3]
     ```
     """
     return array[:till]
@@ -364,7 +352,7 @@ def uniq(array: list) -> list:
     Returns a list of unique elements of the array
     ```py
     >>> lowdash.uniq([1, 2, 3, 4, 5, 5, 4, 4])
-    >>> [1, 2, 3, 4, 5]
+    [1, 2, 3, 4, 5]
     ```
     """
     return list(set(array))
@@ -376,10 +364,10 @@ def without(array: list, *args) -> list:
     Removes the elements of the array passed in from the array
     ```py
     >>> lowdash.without([1, 2, 3, 4, 5], 2, 3)
-    >>> [1, 4, 5]
+    [1, 4, 5]
     ```
     """
-    return [i for i in array if i not in args and i not in args]
+    return [i for i in array if i not in args]
 
 
 @args_type_checker
@@ -388,10 +376,10 @@ def shift(array: list) -> list:
     Similar to `tail` function removes the first element of the array
     ```py
     >>> lowdash.shift([1, 2, 3, 4, 5])
-    >>> [2, 3, 4, 5]
+    [2, 3, 4, 5]
     ```
     """
-    if len(array) != 0:
+    if len(array):
         array.pop(0)
 
     return array
@@ -403,7 +391,7 @@ def unshift(array: list, value) -> list:
     Adds an element to the beginning of the array
     ```py
     >>> lowdash.unshift([1, 2, 3, 4, 5], 6)
-    >>> [6, 1, 2, 3, 4, 5]
+    [6, 1, 2, 3, 4, 5]
     ```
     """
     return array.insert(0, value)
@@ -415,7 +403,7 @@ def union(array: list, *args) -> list:
     Returns a list of union elements of the array and arguments passed in
     ```py
     >>> lowdash.union([1, 2, 3, 4, 5], [2, 3, 4, 5, 6])
-    >>> [1, 2, 3, 4, 5, 6]
+    [1, 2, 3, 4, 5, 6]
     ```
     """
     return list(
@@ -431,7 +419,7 @@ def xor(array: list, *args) -> list:
     Returns a list of elements that are in one of the arrays and not in both
     ```py
     >>> lowdash.xor([1, 2, 3, 4, 5], [2, 3, 4, 5, 6])
-    >>> [1, 6]
+    [1, 6]
     ```
     """
     return list(
@@ -457,6 +445,6 @@ def zip(array: list, *args) -> list:
     Example:
     .. code-block::python
         >>> lowdash.zip([1, 2, 3, 4, 5], [6, 7, 8, 9, 10])
-        >>> [(1, 6), (2, 7), (3, 8), (4, 9), (5, 10)]
+        [(1, 6), (2, 7), (3, 8), (4, 9), (5, 10)]
     """
     return list(zip(array, *args))
